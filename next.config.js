0d4c99b0bdadc72b3f2c3a9372027d11f434b465
inishantxchandel <nishantxchandel@gmail.com>
2023-09-11 15:17:02 +0530

    fixed env

--- next.config.js
+++ next.config.js
@@ -13,6 +13,19 @@ const nextConfig = {
       topLevelAwait: true,
     }
     return config
+  },
+  env:{
+    MONGODB_URI: process.env.MONGODB_URI,
+    MONGODB_DB: process.env.MONGODB_DB,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL_INTERNAL: process.env.NEXTAUTH_URL_INTERNAL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_SIGNING_PRIVATE_KEY: process.env.JWT_SIGNING_PRIVATE_KEY,
+    JWT_SIGNING_PRIVATE_KEY_ID: process.env.JWT_SIGNING_PRIVATE_KEY_ID,
+    
   }
 }
 
